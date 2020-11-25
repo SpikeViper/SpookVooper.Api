b3fe8557d96ae8781a5a4be1f59738cb74b9bf3e
BuildTools <kobe.albright@gmail.com>
2020-11-25 17:27:12 -0500

    Add examples

--- SpookVooper.Api/Example.cs
+++ SpookVooper.Api/Example.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using SpookVooper.Api.Entities;
+using System;
 using System.Threading.Tasks;
 
 namespace SpookVooper.Api
@@ -9,8 +7,33 @@ namespace SpookVooper.Api
     /// <summary>
     /// This class exists to show you how to use the API effectively
     /// </summary>
-    class Example
+    public class Example
     {
+        public async Task Main(string[] args)
+        {
+            // Define two users with SVIDs
+            User spike = new User("u-2a0057e6-356a-4a49-b825-c37796cb7bd9");
+            User brendan = new User("u-02c977bb-0a6c-4eb2-bfca-5e9101025aaf");
 
+            // Print their names and balances
+            Console.WriteLine($"{spike.GetUsername()} has ¢{spike.GetBalance()}");
+            Console.WriteLine($"{brendan.GetUsername()} has ¢{brendan.GetBalance()}");
+
+            // Set the key for spike *can also be done as a second argument during creation*
+            spike.SetAuthKey("this-is-a-key");
+
+            // Have spike send a transaction to brendan
+            TaskResult result = await spike.SendCredits(100, brendan, "Friend payment");
+
+            // Log the result of the transaction
+            Console.WriteLine(result.Info);
+
+            // Need more data?
+            // Use "snapshots" to get a large yet non-updating set of data from a group or user
+            UserSnapshot snapShot = await spike.GetSnapshot();
+            int messageCount = snapShot.discord_message_count;
+
+            Console.WriteLine($"{spike.GetUsername()} sent {messageCount} messages!");
+        }
     }
 }
