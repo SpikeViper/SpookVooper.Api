69e54552dceaf20043de89428260e184a9de3aea
BuildTools <kobe.albright@gmail.com>
2020-11-27 20:23:13 -0500

    Added transaction hub handling

--- SpookVooper.Api/Economy/Transaction.cs
+++ SpookVooper.Api/Economy/Transaction.cs
@@ -1,18 +1,33 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SpookVooper.Api.Economy
 {
+    public enum ApplicableTax
+    {
+        None = 0, Corporate = 1, Payroll = 2, CapitalGains = 3, Sales = 4
+    }
+
     public class Transaction
     {
-        [Key]
-        public string ID { get; set; }
-        public decimal Credits { get; set; }
-        public DateTime Time { get; set; }
-        public string FromUser { get; set; }
-        public string ToUser { get; set; }
-        public string Details { get; set; }
+        [JsonProperty("FromAccount")]
+        public string FromAccount;
+        [JsonProperty("ToAccount")]
+        public string ToAccount;
+        [JsonProperty("Amount")]
+        public decimal Amount;
+        [JsonProperty("Detail")]
+        public string Detail;
+        [JsonProperty("Force")]
+        public bool Force;
+        [JsonProperty("IsCompleted")]
+        private bool IsCompleted;
+        [JsonProperty("Tax")]
+        public ApplicableTax Tax;
+        [JsonProperty("Result")]
+        private TaskResult Result;
     }
 }
---
+++ SpookVooper.Api/Economy/TransactionHub.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpookVooper.Api.Economy
+{
+    /// <summary>
+    /// The function to be run when a transaction is recieved
+    /// </summary>
+    public delegate void TransactionAction(Transaction transaction);
+
+    /// <summary>
+    /// A class used to connect to and use the transaction hub system
+    /// </summary>
+    class TransactionHub
+    {
+        public HubConnection connection;
+
+        public event TransactionAction OnTransaction;
+
+        public TransactionHub()
+        {
+            Console.WriteLine("Starting Transaction hub connection...");
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://spookvooper.com/transactionHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Closed += OnClosed;
+
+            connection.On("NotifyTransaction", (string message) =>
+            {
+                Transaction transaction = JsonConvert.DeserializeObject<Transaction>(message);
+                OnTransaction?.Invoke(transaction);
+            });
+
+            try
+            {
+                connection.StartAsync();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("En error occured while opening the SignalR for the Transaction hub");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public async Task OnClosed(Exception e)
+        {
+            Console.WriteLine("Transaction hub disconnected...");
+            Console.WriteLine(e.Message);
+
+            await connection.StartAsync();
+        }
+    }
+}
